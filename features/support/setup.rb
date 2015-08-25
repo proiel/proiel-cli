1ae241db0c3e8a9e7fdb2cedb43054a7b4fa256b
Dag Haug <daghaug@ifikk.uio.no>
2015-08-25 14:48:07 +0200

    Tests for CoNLL-U conversion

--- features/support/setup.rb
+++ features/support/setup.rb
@@ -4,3 +4,16 @@ Then(/^the output should be equal to file "([^"]*)"$/) do |reference_file|
   expected = File.open(reference_file).read
   assert_exact_output(expected, all_stdout)
 end
+
+Then(/^the output should have (\d+) lines$/) do |n|
+  File.open("tmp.fil", "w").write(all_stdout)
+  assert_exact_output(n, all_stdout.split("\n").length.to_s)
+end
+
+Then(/^the output should be empty$/) do
+  assert_exact_output('', all_stdout)
+end
+
+Then(/^the errors should contain "([^"]*)"$/) do | pattern|
+  assert_matching_output(pattern, all_stderr)
+end
