69334f08e1ae3ca66b06d876a95f9cb68a747bca
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2018-02-18 18:34:41 +0000

    Add dictionary command

---
+++ lib/proiel/cli/commands/dictionary.rb
@@ -0,0 +1,46 @@
+module PROIEL
+  module Commands
+    class Dictionary < Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:dictionary) do |c|
+            c.syntax 'dictionary [options] filename(s)'
+            c.description 'Build a dictionary'
+
+            c.option 'glosses', '--merge-glosses glosses.tsv', 'Merge glosses from an external file'
+            c.option 'gloss-languages', '--merge-gloss-languages eng,rus', 'Merge glosses from selected languages'
+
+            c.action { |args, options| process(args, options) }
+          end
+        end
+
+        def process(args, options)
+          tb = PROIEL::Treebank.new
+          dict = PROIEL::Dictionary::Builder.new
+
+          args.each do |filename|
+            STDERR.puts "Reading #{filename}...".green if options['verbose']
+
+            tb.load_from_xml(filename)
+          end
+
+          if options['glosses']
+            languages = (options['gloss-languages'] || 'eng').split(',').map(&:to_sym)
+            if File.exists?(options['glosses'])
+              dict.add_external_glosses!(options['glosses'], languages)
+            else
+              STDERR.puts "#{options['glosses']} not found"
+              exit 1
+            end
+          end
+
+          tb.sources.each do |source|
+            dict.add_source!(source)
+          end
+
+          dict.to_xml(STDOUT)
+        end
+      end
+    end
+  end
+end
